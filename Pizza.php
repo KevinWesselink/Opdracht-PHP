6c1da9e8f61e63f4f08645ca176d576750bb5bf8
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-10-06 16:35:16 +0200

    Pizza.php aangemaakt.

---
+++ Pizza.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css?=v061020221631">
+    <title>Lekker pizza's bestellen</title>
+</head>
+<body>
+    <nav>
+        <a href="./index.php">Hoofdstuk 2</a>
+        <a href="./Hoofdstuk%203.php">Hoofdstuk 3</a>
+        <a href="./Pizza.php">Pizza</a>
+        <a href="./Rente.php">Rente</a>
+    </nav>
+
+</body>
+</html>
