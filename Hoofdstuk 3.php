a31d3cfb7f7b6b305de6056ea760683df74f5e57
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-10-06 16:34:58 +0200

    Navbar toegevoegd aan index.php.

--- Hoofdstuk 3.php
+++ Hoofdstuk 3.php
@@ -20,6 +20,8 @@ if (isset($_POST["submit"])) {
     <nav>
         <a href="./index.php">Hoofdstuk 2</a>
         <a href="./Hoofdstuk%203.php">Hoofdstuk 3</a>
+        <a href="./Pizza.php">Pizza</a>
+        <a href="./Rente.php">Rente</a>
     </nav>
 
     <form method="post" action="">
@@ -62,27 +64,6 @@ if (isset($_POST["submit"])) {
             default:
                 echo 'U moet nog een taal kiezen!';
         }
-//        if ($land == 'CHOICE') {
-//            echo 'U moet nog een taal kiezen!';
-//        }
-//        elseif ($land == 'NL') {
-//            echo 'Goedemorgen' . ' ' . $naam;
-//        }
-//        elseif ($land == 'DE') {
-//            echo 'Guten Morgen' . ' ' . $naam;
-//        }
-//        elseif ($land == 'EN') {
-//            echo 'Good morning' . ' ' . $naam;
-//        }
-//        elseif ($land == 'FR') {
-//            echo 'Bonjour' . ' ' . $naam;
-//        }
-//        elseif ($land == 'ES') {
-//            echo 'Buenos dìas' . ' ' . $naam;
-//        }
-//        elseif ($land == 'IT') {
-//            echo 'Buongiorno' . ' ' .  $naam;
-//        }
     }
     ?>
 
