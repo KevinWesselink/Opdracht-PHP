34257400b412569912852b25bd10d39c71fc7212
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-10-10 13:05:31 +0200

    Poging gedaan om met arrays te werken.

--- Pizza.php
+++ Pizza.php
@@ -4,14 +4,14 @@ $address = '';
 $postcode = '';
 $location = '';
 $orderDate = date("d/m/Y");
-echo $orderDate;
 $deliveryMethod = '';
-$pizza = '';
-$pizza1 = 12.50;
-$pizza2 = 12.50;
-$pizza3 = 13.95;
-$pizza4 = 11.50;
-$pizza5 = 14.50;
+$pizza = array (
+        array("Pizza Margherita", 12.50),
+        array("Pizza Funghi", 12.50),
+        array("Pizza Marina", 13.95),
+        array("Pizza Hawai", 11.50),
+        array("Pizza Quattro Formaggi", 14.50)
+);
 if (isset($_POST["submit"])) {
     $name = $_POST["name"];
     $address = $_POST["address"];
@@ -54,20 +54,52 @@ if (isset($_POST["submit"])) {
             Plaats: <input type="text" name="location" placeholder="Plaats" value="<?php echo $location?>" required minlength="2" maxlength="20"><br>
 
             Pizza's: <br>
-            <input type="checkbox" id="pizzaMargherita" name="pizza" value="<?php $pizza1?>">
-            <label for="pizzaMargherita">Pizza Margherita - €12,50</label><br>
-
-            <input type="checkbox" id="pizzaFunghi" name="pizza" value="<?php echo "Pizza Funghi"?>">
-            <label for="pizzaFunghi">Pizza Funghi - €12,50</label><br>
-
-            <input type="checkbox" id="pizzaMarina" name="pizza" value="<?php echo "Pizza Marina"?>">
-            <label for="pizzaMarina">Pizza Marina - €13,95</label><br>
-
-            <input type="checkbox" id="pizzaHawai" name="pizza" value="<?php echo "Pizza Hawai"?>">
-            <label for="pizzaHawai">Pizza Hawai - €11,50</label><br>
-
-            <input type="checkbox" id="pizzaQuattroFormaggi" value="<?php echo "Pizza Quattro Formaggi"?>">
-            <label for="pizzaQuattroFormaggi">Pizza Quattro Formaggi - €14,50</label><br>
+            <table>
+                <tr>
+                    <th>Pizza naam</th>
+                    <th>Pizza prijs</th>
+                    <th>Aantal pizza's</th>
+                </tr>
+                <tr>
+                    <td><?php echo $pizza[0][0];?></td>
+                    <td><?php echo $pizza[0][1];?></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><?php echo $pizza[1][0];?></td>
+                    <td><?php echo $pizza[1][1];?></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><?php echo $pizza[2][0];?></td>
+                    <td><?php echo $pizza[2][1];?></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><?php echo $pizza[3][0];?></td>
+                    <td><?php echo $pizza[3][1];?></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><?php echo $pizza[4][0];?></td>
+                    <td><?php echo $pizza[4][1];?></td>
+                    <td></td>
+                </tr>
+            </table>
+<!--            <input type="checkbox" id="pizzaMargherita" name="pizza" value="--><?php //echo $pizza[0][0]?><!--">-->
+<!--            <label for="pizzaMargherita">Pizza Margherita - €12,50</label><br>-->
+<!---->
+<!--            <input type="checkbox" id="pizzaFunghi" name="pizza" value="--><?php //echo $pizza[1][0]?><!--">-->
+<!--            <label for="pizzaFunghi">Pizza Funghi - €12,50</label><br>-->
+<!---->
+<!--            <input type="checkbox" id="pizzaMarina" name="pizza" value="--><?php //echo $pizza[2][0]?><!--">-->
+<!--            <label for="pizzaMarina">Pizza Marina - €13,95</label><br>-->
+<!---->
+<!--            <input type="checkbox" id="pizzaHawai" name="pizza" value="--><?php //echo $pizza[3][0]?><!--">-->
+<!--            <label for="pizzaHawai">Pizza Hawai - €11,50</label><br>-->
+<!---->
+<!--            <input type="checkbox" id="pizzaQuattroFormaggi" value="--><?php //echo $pizza[4][0]?><!--">-->
+<!--            <label for="pizzaQuattroFormaggi">Pizza Quattro Formaggi - €14,50</label><br>-->
 
             Bezorgen of ophalen:
             <input type="radio" id="bezorgen" name="deliveryMethod" value="<?php echo "bezorgen"?>" required>
